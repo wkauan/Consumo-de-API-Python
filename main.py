085fb19097e83f2bf5bee53306e2cab1ed140953
Kauan Bomfim <111811804+wkauan@users.noreply.github.com>
2023-09-06 19:53:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import requests
 def consulta_cep():
     cep = input('Informe um CEP: ')
 
+    cep = cep.replace("-", "").replace(".", "").replace(" ", "")
+
     consulta = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 
     if consulta.status_code == 200:
         requisicao = consulta.json()
-        if 'erro' in requisicao:
-            consulta.status_code = 404
-        
+        if 'erro' in requisicao:      
             print('O status code é: ', consulta.status_code)
 
             print('CEP Não encontrado \n')
@@ -17,15 +17,16 @@ def consulta_cep():
             print('O status code é: ', consulta.status_code)
 
             uf = requisicao['uf']
-            rua = requisicao['logradouro']
+            endereco = requisicao['logradouro']
             bairro = requisicao['bairro']
             ddd = requisicao['ddd']
+            cidade = requisicao['localidade']
 
             print('Resultado da busca: \n', )
 
-            print('O CEP informado foi: %s.' % cep)
+            print('O CEP informado foi: %s.' % requisicao['cep'])
 
-            print('Rua: %s, Bairro: %s, UF: %s, DDD: %s \n' % (rua, bairro, uf, ddd))
+            print('Endereço: %s, %s, %s - %s DDD: %s \n' % (endereco, bairro, cidade, uf, ddd))
     elif consulta.status_code == 400:
         print('O status code é: ', consulta.status_code)
 

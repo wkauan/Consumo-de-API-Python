4251a0167c3966377eede01cb1e2fc6d4ebc2c52
wkauan <kauanmbomfim5@gmail.com>
2023-09-15 10:05:30 -0300

    :recycle: rafctor: removendo a variavel menssagem

--- main.py
+++ main.py
@@ -9,24 +9,23 @@ def consulta_cep():
     consulta = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 
     return consulta
-    
+
 
 def tratamento_erro(consulta):
     if consulta.status_code == 200:
         requisicao = consulta.json()
         if 'erro' in requisicao:
             consulta.status_code = 404
-            menssagem = 'CEP não encontrado \n'
+            print('CEP não encontrado \n')
         else:
-            menssagem = 'Resultado da busca: \n'
+            print('Resultado da busca: \n')
 
-            menssagem = f'O CEP informado foi: {requisicao["cep"]}.'
+            print(f'O CEP informado foi: {requisicao["cep"]}.')
 
-            menssagem = f'Endereço: {requisicao["logradouro"]}, {requisicao["bairro"]}, {requisicao["localidade"]} - {requisicao["uf"]} DDD: {requisicao["ddd"]} \n'
+            print(f'Endereço: {requisicao["logradouro"]}, {requisicao["bairro"]}, {requisicao["localidade"]} - {requisicao["uf"]} DDD: {requisicao["ddd"]} \n')
     else:
-        menssagem = 'CEP Inválido \n'
+        print('CEP Inválido \n')
 
     print('O status code é: ', consulta.status_code)
-    print(menssagem)
 
 tratamento_erro(consulta_cep())
